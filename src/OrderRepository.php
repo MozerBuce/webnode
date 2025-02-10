3cd84c6f5d6a227be3268ca882266f0cc3c92917
Almerino Buce <AlmerinoBuce998@Gmail.com>
2025-02-10 16:19:15 +0100

    add the  Order Repository

---
+++ src/OrderRepository.php
@@ -0,0 +1,43 @@
+<?php
+require_once 'Order.php';
+require_once 'OrderItem.php';
+require_once __DIR__ . '/../config/database.php';
+
+// This class is responsible for database operations related to Order entities.
+
+class OrderRepository
+{
+
+    private $db;
+
+    // Initialize connection using Database class
+    public function __construct()
+    {
+        $this->db = (new Database())->connect();
+    }
+
+    //  Function to Fetch the order based on the id
+    public function getOrderById($id)
+    {
+
+        // Selecting data based on the id from the table orders
+        $query = "SELECT * FROM orders WHERE id = ?";
+        $stmt = $this->db->prepare($query);
+        $stmt->execute([$id]);
+        $order = $stmt->fetch(PDO::FETCH_ASSOC);
+
+        // In case no data is found it returns null
+        if (!$order) {
+            return null;
+        }
+
+        // Fetching the order items related to the given order id
+        $itemsQuery = "SELECT * FROM order_items WHERE order_id = ?";
+        $stmt = $this->db->prepare($itemsQuery);
+        $stmt->execute([$id]);
+        $order['items'] = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+        // return the data
+        return $order;
+    }
+}
